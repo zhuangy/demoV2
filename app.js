25071e9ec944716b4144af33882e3a10d85ed0a6
Masha Belyi <mashabelyi@gmail.com>
2012-11-19 14:44:33 -0800

    enabling compression

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ var dust = require('dustjs-linkedin');
 var cons = require('consolidate');
 
 
-var app = express.createServer();
+var app = express();
 
 app.configure(function() {
-  app.register('dust', cons.dust);
-  app.use(express.static(__dirname+'/static'));
+  //app.register('dust', cons.dust);
+
   app.use(express.compress());
-  app.use(express.bodyParser());
+  //app.use(express.logger());
+  app.use(express.static(__dirname+'/static'));
   app.use(express.methodOverride());
+  app.use(express.bodyParser());
+
   app.use(app.router);
   app.set('view options', {layout: false});
   app.set('view engine', 'dust');
-  
+
 });
 
 app.listen(8000);
