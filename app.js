d54049295cd5211ef0ecfe48ffa948bf2cca9717
Masha Belyi <mashabelyi@gmail.com>
2012-11-28 18:15:55 -0800

    add 1 day caching

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 var express = require('express');
 var dust = require('dustjs-linkedin');
 var cons = require('consolidate');
-var expressUglify = require('express-uglify');
-
+var gzippo = require('gzippo');
 var app = express();
 
+
 app.configure(function() {
-  //app.register('dust', cons.dust);
-
-  app.use(express.compress());
-  app.use(expressUglify.middleware({
-        src : __dirname + '/static',
-        logLevel: 'none',
-        }));
-
-  app.use(express.static(__dirname+'/static'));
-  app.use(express.methodOverride());
-  app.use(express.bodyParser());
-
-  app.use(app.router);
-  app.set('view options', {layout: false});
-  app.set('view engine', 'dust');
+        app.use(gzippo.staticGzip(__dirname + '/static'));
+        //app.use(gzippo.compress());
+        app.use(express.methodOverride());
+        app.use(express.bodyParser());
+        app.use(app.router);
+        app.set('view options', {
+                layout: false
+        });
+        app.set('view engine', 'dust');
 });
 
 app.listen(8000);
+
